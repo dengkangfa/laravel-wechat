a10f21ccaf99f2bf2636ae735935ec4d7cad3ec5
vagrant <vagrant@homestead>
2017-09-27 17:19:14 +0000

    第14次测试自动部署功能

--- public/git-webhook-handler.php
+++ public/git-webhook-handler.php
@@ -8,14 +8,11 @@ if (empty($requestBody)) {
 }
 $contents = json_decode($requestBody, true);
 if ($contents['ref'] == 'refs/heads/master') {
-    echo 1;
     $res = shell_exec("cd {$path} && git pull 2>&1");
-        echo 2;
     $res_log = '------------------------'.PHP_EOL;
     $res_log .= $contents['pusher']['name'] . ' 在' . data('Y-m-d H:i:s') . '向' . $contents['repository']['name'] . '项目的'
         . $contents['ref'] . '分支push了' . (isset($contents['total_commits_count']) ? $contents['total_commits_count'] : 1)  . '个commit:' . PHP_EOL;
     $res_log .= $res.PHP_EOL;
-        echo 3;
     $result = file_put_contents('git-webhook.txt', $res_log, FILE_APPEND);
-    echo $result.'=>'.$res;
+    echo $result;
 }
